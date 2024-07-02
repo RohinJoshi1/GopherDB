54fb8fb853ef6cb0dfc2fcc1d4451044e4056298
Rohin Joshi <rrohiinujoshi@gmail.com>
2024-07-02 17:50:41 +0530

    Adding db insert

---
+++ collection.go
@@ -0,0 +1,133 @@
+package main
+
+import "bytes"
+
+type Collection struct {
+	name []byte
+	root pgnum
+
+	dal *dal
+}
+
+func newCollection(name []byte, root pgnum) *Collection {
+	return &Collection{
+		name: name,
+		root: root,
+	}
+}
+
+// Put adds a key to the tree. It finds the correct node and the insertion index and adds the item. When performing the
+// search, the ancestors are returned as well. This way we can iterate over them to check which nodes were modified and
+// rebalance by splitting them accordingly. If the root has too many items, then a new root of a new layer is
+// created and the created nodes from the split are added as children.
+func (c *Collection) Put(key []byte, value []byte) error {
+	i := newItem(key, value)
+
+	// On first insertion the root node does not exist, so it should be created
+	var root *Node
+	var err error
+	if c.root == 0 {
+		root, err = c.dal.writeNode(c.dal.newNode([]*Item{i}, []pgnum{}))
+		if err != nil {
+			return nil
+		}
+		c.root = root.pageNum
+		return nil
+	} else {
+		root, err = c.dal.getNode(c.root)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Find the path to the node where the insertion should happen
+	insertionIndex, nodeToInsertIn, ancestorsIndexes, err := root.findKey(i.key, false)
+	if err != nil {
+		return err
+	}
+
+	// If key already exists
+	if nodeToInsertIn.items != nil && insertionIndex < len(nodeToInsertIn.items) && bytes.Equal(nodeToInsertIn.items[insertionIndex].key, key) {
+		nodeToInsertIn.items[insertionIndex] = i
+	} else {
+		// Add item to the leaf node
+		nodeToInsertIn.addItem(i, insertionIndex)
+	}
+	nodeToInsertIn.writeNode(nodeToInsertIn)
+
+	ancestors, err := c.getNodes(ancestorsIndexes)
+	if err != nil {
+		return err
+	}
+
+	// Rebalance the nodes all the way up. Start From one node before the last and go all the way up. Exclude root.
+	for i := len(ancestors) - 2; i >= 0; i-- {
+		pnode := ancestors[i]
+		node := ancestors[i+1]
+		nodeIndex := ancestorsIndexes[i+1]
+		if node.isOverPopulated() {
+			pnode.split(node, nodeIndex)
+		}
+	}
+
+	// Handle root
+	rootNode := ancestors[0]
+	if rootNode.isOverPopulated() {
+		newRoot := c.dal.newNode([]*Item{}, []pgnum{rootNode.pageNum})
+		newRoot.split(rootNode, 0)
+
+		// commit newly created root
+		newRoot, err = c.dal.writeNode(newRoot)
+		if err != nil {
+			return err
+		}
+
+		c.root = newRoot.pageNum
+	}
+
+	return nil
+}
+
+// Find Returns an item according based on the given key by performing a binary search.
+func (c *Collection) Find(key []byte) (*Item, error) {
+	n, err := c.dal.getNode(c.root)
+	if err != nil {
+		return nil, err
+	}
+
+	index, containingNode, _, err := n.findKey(key, true)
+	if err != nil {
+		return nil, err
+	}
+	if index == -1 {
+		return nil, nil
+	}
+	return containingNode.items[index], nil
+}
+
+// getNodes returns a list of nodes based on their indexes (the breadcrumbs) from the root
+//
+//	         p
+//	     /       \
+//	   a          b
+//	/     \     /   \
+//
+// c       d   e     f
+// For [0,1,0] -> p,b,e
+func (c *Collection) getNodes(indexes []int) ([]*Node, error) {
+	root, err := c.dal.getNode(c.root)
+	if err != nil {
+		return nil, err
+	}
+
+	nodes := []*Node{root}
+	child := root
+	for i := 1; i < len(indexes); i++ {
+		child, err = c.dal.getNode(child.childNodes[indexes[i]])
+		if err != nil {
+			return nil, err
+		}
+		nodes = append(nodes, child)
+	}
+	return nodes, nil
+}
---
+++ node.go
@@ -0,0 +1,309 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+type Item struct {
+	key   []byte
+	value []byte
+}
+
+type Node struct {
+	*dal
+
+	pageNum    pgnum
+	items      []*Item
+	childNodes []pgnum
+}
+
+func NewEmptyNode() *Node {
+	return &Node{}
+}
+
+// NewNodeForSerialization creates a new node only with the properties that are relevant when saving to the disk
+func NewNodeForSerialization(items []*Item, childNodes []pgnum) *Node {
+	return &Node{
+		items:      items,
+		childNodes: childNodes,
+	}
+}
+
+func newItem(key []byte, value []byte) *Item {
+	return &Item{
+		key:   key,
+		value: value,
+	}
+}
+
+func isLast(index int, parentNode *Node) bool {
+	return index == len(parentNode.items)
+}
+
+func isFirst(index int) bool {
+	return index == 0
+}
+
+func (n *Node) isLeaf() bool {
+	return len(n.childNodes) == 0
+}
+
+func (n *Node) writeNode(node *Node) *Node {
+	node, _ = n.dal.writeNode(node)
+	return node
+}
+
+func (n *Node) writeNodes(nodes ...*Node) {
+	for _, node := range nodes {
+		n.writeNode(node)
+	}
+}
+
+func (n *Node) getNode(pageNum pgnum) (*Node, error) {
+	return n.dal.getNode(pageNum)
+}
+
+// isOverPopulated checks if the node size is bigger than the size of a page.
+func (n *Node) isOverPopulated() bool {
+	return n.dal.isOverPopulated(n)
+}
+
+// canSpareAnElement checks if the node size is big enough to populate a page after giving away one item.
+func (n *Node) canSpareAnElement() bool {
+	splitIndex := n.dal.getSplitIndex(n)
+	if splitIndex == -1 {
+		return false
+	}
+	return true
+}
+
+// isUnderPopulated checks if the node size is smaller than the size of a page.
+func (n *Node) isUnderPopulated() bool {
+	return n.dal.isUnderPopulated(n)
+}
+
+func (n *Node) serialize(buf []byte) []byte {
+	leftPos := 0
+	rightPos := len(buf) - 1
+
+	// Add page header: isLeaf, key-value pairs count, node num
+	// isLeaf
+	isLeaf := n.isLeaf()
+	var bitSetVar uint64
+	if isLeaf {
+		bitSetVar = 1
+	}
+	buf[leftPos] = byte(bitSetVar)
+	leftPos += 1
+
+	// key-value pairs count
+	binary.LittleEndian.PutUint16(buf[leftPos:], uint16(len(n.items)))
+	leftPos += 2
+
+	//slotted pages for storing data in the page. It means the actual keys and values (the cells) are appended
+	// to right of the page whereas offsets have a fixed size and are appended from the left.
+
+	for i := 0; i < len(n.items); i++ {
+		item := n.items[i]
+		if !isLeaf {
+			childNode := n.childNodes[i]
+
+			// Write the child page as a fixed size of 8 bytes
+			binary.LittleEndian.PutUint64(buf[leftPos:], uint64(childNode))
+			leftPos += pageNumSize
+		}
+
+		klen := len(item.key)
+		vlen := len(item.value)
+
+		// write offset
+		offset := rightPos - klen - vlen - 2
+		binary.LittleEndian.PutUint16(buf[leftPos:], uint16(offset))
+		leftPos += 2
+
+		rightPos -= vlen
+		copy(buf[rightPos:], item.value)
+
+		rightPos -= 1
+		buf[rightPos] = byte(vlen)
+
+		rightPos -= klen
+		copy(buf[rightPos:], item.key)
+
+		rightPos -= 1
+		buf[rightPos] = byte(klen)
+	}
+
+	if !isLeaf {
+		// Write the last child node
+		lastChildNode := n.childNodes[len(n.childNodes)-1]
+		// Write the child page as a fixed size of 8 bytes
+		binary.LittleEndian.PutUint64(buf[leftPos:], uint64(lastChildNode))
+	}
+
+	return buf
+}
+
+func (n *Node) deserialize(buf []byte) {
+	leftPos := 0
+
+	// Read header
+	isLeaf := uint16(buf[0])
+
+	itemsCount := int(binary.LittleEndian.Uint16(buf[1:3]))
+	leftPos += 3
+
+	// Read body
+	for i := 0; i < itemsCount; i++ {
+		if isLeaf == 0 { // False
+			pageNum := binary.LittleEndian.Uint64(buf[leftPos:])
+			leftPos += pageNumSize
+
+			n.childNodes = append(n.childNodes, pgnum(pageNum))
+		}
+
+		// Read offset
+		offset := binary.LittleEndian.Uint16(buf[leftPos:])
+		leftPos += 2
+
+		klen := uint16(buf[int(offset)])
+		offset += 1
+
+		key := buf[offset : offset+klen]
+		offset += klen
+
+		vlen := uint16(buf[int(offset)])
+		offset += 1
+
+		value := buf[offset : offset+vlen]
+		offset += vlen
+		n.items = append(n.items, newItem(key, value))
+	}
+
+	if isLeaf == 0 { // False
+		// Read the last child node
+		pageNum := pgnum(binary.LittleEndian.Uint64(buf[leftPos:]))
+		n.childNodes = append(n.childNodes, pageNum)
+	}
+}
+
+// elementSize returns the size of a key-value-childNode triplet at a given index.
+// If the node is a leaf, then the size of a key-value pair is returned.
+// It's assumed i <= len(n.items)
+func (n *Node) elementSize(i int) int {
+	size := 0
+	size += len(n.items[i].key)
+	size += len(n.items[i].value)
+	size += pageNumSize // 8 is the pgnum size
+	return size
+}
+
+// nodeSize returns the node's size in bytes
+func (n *Node) nodeSize() int {
+	size := 0
+	size += nodeHeaderSize
+
+	for i := range n.items {
+		size += n.elementSize(i)
+	}
+
+	// Add last page
+	size += pageNumSize // 8 is the pgnum size
+	return size
+}
+
+// findKey searches for a key inside the tree. Once the key is found, the parent node and the correct index are returned
+// so the key itself can be accessed in the following way parent[index]. A list of the node ancestors (not including the
+// node itself) is also returned.
+// If the key isn't found, we have 2 options. If exact is true, it means we expect findKey
+// If exact is false, then findKey is used to locate where a new key should be
+// inserted so the position is returned.
+func (n *Node) findKey(key []byte, exact bool) (int, *Node, []int, error) {
+	ancestorsIndexes := []int{0} // index of root
+	index, node, err := findKeyHelper(n, key, exact, &ancestorsIndexes)
+	if err != nil {
+		return -1, nil, nil, err
+	}
+	return index, node, ancestorsIndexes, nil
+}
+
+func findKeyHelper(node *Node, key []byte, exact bool, ancestorsIndexes *[]int) (int, *Node, error) {
+	wasFound, index := node.findKeyInNode(key)
+	if wasFound {
+		return index, node, nil
+	}
+
+	if node.isLeaf() {
+		if exact {
+			return -1, nil, nil
+		}
+		return index, node, nil
+	}
+
+	*ancestorsIndexes = append(*ancestorsIndexes, index)
+	nextChild, err := node.getNode(node.childNodes[index])
+	if err != nil {
+		return -1, nil, err
+	}
+	return findKeyHelper(nextChild, key, exact, ancestorsIndexes)
+}
+
+// findKeyInNode iterates all the items and finds the key. If the key is found, then the item is returned. If the key
+// isn't found then return the index where it should have been (the first index that key is greater than it's previous)
+func (n *Node) findKeyInNode(key []byte) (bool, int) {
+	for i, existingItem := range n.items {
+		res := bytes.Compare(existingItem.key, key)
+		if res == 0 { // Keys match
+			return true, i
+		}
+
+		// The key is bigger than the previous item, so it doesn't exist in the node, but may exist in child nodes.
+		if res == 1 {
+			return false, i
+		}
+	}
+
+	// The key isn't bigger than any of the items which means it's in the last index.
+	return false, len(n.items)
+}
+
+func (n *Node) addItem(item *Item, insertionIndex int) int {
+	if len(n.items) == insertionIndex { // nil or empty slice or after last element
+		n.items = append(n.items, item)
+		return insertionIndex
+	}
+
+	n.items = append(n.items[:insertionIndex+1], n.items[insertionIndex:]...)
+	n.items[insertionIndex] = item
+	return insertionIndex
+}
+
+// split rebalances the tree after adding. After insertion the modified node has to be checked to make sure it
+// didn't exceed the maximum number of elements. If it did, then it has to be split and rebalanced. 
+func (n *Node) split(nodeToSplit *Node, nodeToSplitIndex int) {
+	// The first index where min amount of bytes to populate a page is achieved. Then add 1 so it will be split one
+	// index after.
+	splitIndex := nodeToSplit.dal.getSplitIndex(nodeToSplit)
+
+	middleItem := nodeToSplit.items[splitIndex]
+	var newNode *Node
+
+	if nodeToSplit.isLeaf() {
+		newNode = n.writeNode(n.dal.newNode(nodeToSplit.items[splitIndex+1:], []pgnum{}))
+		nodeToSplit.items = nodeToSplit.items[:splitIndex]
+	} else {
+		newNode = n.writeNode(n.dal.newNode(nodeToSplit.items[splitIndex+1:], nodeToSplit.childNodes[splitIndex+1:]))
+		nodeToSplit.items = nodeToSplit.items[:splitIndex]
+		nodeToSplit.childNodes = nodeToSplit.childNodes[:splitIndex+1]
+	}
+	n.addItem(middleItem, nodeToSplitIndex)
+	if len(n.childNodes) == nodeToSplitIndex+1 { // If middle of list, then move items forward
+		n.childNodes = append(n.childNodes, newNode.pageNum)
+	} else {
+		n.childNodes = append(n.childNodes[:nodeToSplitIndex+1], n.childNodes[nodeToSplitIndex:]...)
+		n.childNodes[nodeToSplitIndex+1] = newNode.pageNum
+	}
+
+	n.writeNodes(n, nodeToSplit)
+}
